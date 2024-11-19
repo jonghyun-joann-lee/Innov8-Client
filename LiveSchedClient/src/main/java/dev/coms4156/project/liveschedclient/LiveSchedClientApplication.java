5e89b9f620bbad3ea219ee41afe5d4dd2c55e336
Jonghyun (Joann) Lee <151266186+jonghyun-joann-lee@users.noreply.github.com>
2024-11-19 01:10:55 -0500

    Create MainController and LiveSchedService classes (#1)

--- LiveSchedClient/src/main/java/dev/coms4156/project/liveschedclient/LiveSchedClientApplication.java
+++ LiveSchedClient/src/main/java/dev/coms4156/project/liveschedclient/LiveSchedClientApplication.java
@@ -3,11 +3,19 @@ package dev.coms4156.project.liveschedclient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * This class contains the startup logic for the client application.
+ */
 @SpringBootApplication
 public class LiveSchedClientApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LiveSchedClientApplication.class, args);
-	}
-
+  /**
+   * The main launcher for the client application.
+   * All it does is make a call to the overridden run method.
+   *
+   * @param args A {@code String[]} of any potential runtime arguments
+   */
+  public static void main(String[] args) {
+    SpringApplication.run(LiveSchedClientApplication.class, args);
+  }
 }
